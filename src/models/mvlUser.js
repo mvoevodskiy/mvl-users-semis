1fce0fadd2fe4f94ccaf2ca20ebf11fa13712547
mvoevodskiy <mv@aaa0.ru>
2020-02-25 01:56:41 +0300

    Optimized mvlUser model

--- src/models/mvlUser.js
+++ src/models/mvlUser.js
@@ -1,37 +1,31 @@
 module.exports = (Sequelize) => {
-    let user = [{
-        username: {
-            type: Sequelize.STRING,
-            default: 0,
-            allowNull: false,
-        },
-        botUserId: {
-            type: Sequelize.INTEGER,
-            default: 0,
-        },
-        password: {
-            type: Sequelize.STRING,
-            default: '',
-        },
-        salt: {
-            type: Sequelize.STRING(64),
-            default: '',
-        },
-        remoteKey: {
-            type: Sequelize.STRING,
-        },
-        remoteData: {
-            type: Sequelize.TEXT,
-        },
-    },
+    return [
         {
-        classMethods: {
-            associate: function (models) {
-                console.log(this);
-                user.hasMany(models.mvlUserProfile);
-            }
-        }
-    }];
-    return user;
+            username: {
+                type: Sequelize.STRING,
+                default: 0,
+                allowNull: false,
+            },
+            botUserId: {
+                type: Sequelize.INTEGER,
+                default: 0,
+            },
+            password: {
+                type: Sequelize.STRING,
+                default: '',
+            },
+            salt: {
+                type: Sequelize.STRING(64),
+                default: '',
+            },
+            remoteKey: {
+                type: Sequelize.STRING,
+            },
+            remoteData: {
+                type: Sequelize.TEXT,
+            },
+        },
+        {}
+    ];
 
 };
