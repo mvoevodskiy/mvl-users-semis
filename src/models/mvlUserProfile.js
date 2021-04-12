df98bd4479ef44a88256c02bfce359fdb72697c3
mvoevodskiy <mv@aaa0.ru>
2021-04-12 08:24:45 +0300

    Added extended field to User Profile

--- src/models/mvlUserProfile.js
+++ src/models/mvlUserProfile.js
@@ -51,7 +51,29 @@ module.exports = (Sequelize) => {
                 type: Sequelize.STRING,
                 defaultValue: '',
             },
-            language: Sequelize.STRING(3)
+            language: Sequelize.STRING(3),
+            extended: {
+                type: Sequelize.TEXT,
+                defaultValue: '{}',
+                allowNull: false,
+                get () {
+                    try {
+                        return JSON.parse(this.getDataValue('extended'))
+                    } catch (e) {
+                        console.error('GETTER OF extended FIELD OF MODEL mvlUserProfile. RETURN EMPTY OBJECT. ERROR', e)
+                        return {}
+                    }
+                },
+                set (val) {
+                    try {
+                        val = typeof val === 'string' ? val : JSON.stringify(val)
+                    } catch (e) {
+                        console.error('SETTER OF extended FIELD OF MODEL mvlUserProfile. SETTING EMPTY OBJECT. ERROR', e)
+                        return '{}'
+                    }
+                    this.setDataValue('extended', val)
+                }
+            }
         },
         {
             getterMethods: {
